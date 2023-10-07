7d4a628e86e1c15f19572d834495be968f4e7328
Jimmy Li <jli943@Jimmys-MacBook-Pro.local>
2023-10-07 14:13:41 -0400

    lru

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # distributedCache
-# distributedCache
-# distributedCache
+
--- lru/lru.go
+++ lru/lru.go
@@ -1,1 +1,33 @@
 package lru
+
+import "container/list"
+
+// Cache is a LRU cache. It is not safe for concurrent access.
+type Cache struct {
+	maxBytes int64
+	nbytes   int64
+	ll       *list.List
+	cache    map[string]*list.Element
+	// optional and executed when an entry is purged.
+	OnEvicted func(key string, value Value)
+}
+
+type entry struct {
+	key   string
+	value Value
+}
+
+// Value use Len to count how many bytes it takes
+type Value interface {
+	Len() int
+}
+
+// Get look ups a key's value
+func (c *Cache) Get(key string) (value Value, ok bool) {
+	if ele, ok := c.cache[key]; ok {
+		c.ll.MoveToFront(ele)
+		kv := ele.Value.(*entry)
+		return kv.value, true
+	}
+	return
+}
